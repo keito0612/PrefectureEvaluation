de2038f6c39232af3db7531e1bcd366042d479a0
isobe keito <iso309532@gmail.com>
2023-06-22 15:57:10 +0900

    Initial Commit

---
+++ PrefectureEvaluation/PrefectureEvaluationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrefectureEvaluationApp.swift
+//  PrefectureEvaluation
+//
+//  Created by 磯部馨仁 on 2023/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PrefectureEvaluationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
